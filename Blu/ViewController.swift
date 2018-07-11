2651bb8c4a0d25dfdaed52d353af1044ac3b2e05
Leandro Estrada <leandro.a.estrada@gmail.com>
2018-07-11 17:02:29 -0300

    Adicionando funções na View Controller

--- Blu/ViewController.swift
+++ Blu/ViewController.swift
@@ -1,25 +1,47 @@
-//
-//  ViewController.swift
-//  Blu
-//
-//  Created by ITS on 05/07/2018.
-//  Copyright © 2018 leandro. All rights reserved.
-//
-
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn{
+            central.scanForPeripherals(withServices: nil, options: nil)
+            print("Scaneando")
+        }
+    }
+    
+    //Callback para quendo peripheral é descoberto
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.name?.contains("SensorTag") == true{
+        print(peripheral.name ?? "no name")
+        centralManager.stopScan()
+        print(advertisementData)
+        //Para conectar:
+        central.connect(peripheral, options: nil)
+        myPeripheral = peripheral
+       }
+    
+    }
+    //Quando peripheral desconectar
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        central.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("connected \(peripheral.name)")
+    }
+    var centralManager : CBCentralManager!
+    var myPeripheral : CBPeripheral?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager.init(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        }
+
     }
-
-
-}
-
